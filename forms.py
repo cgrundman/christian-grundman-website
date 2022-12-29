0b57893ed5bcea929284cd3bc86bc13d5cb27574
cgrundman <92883095+cgrundman@users.noreply.github.com>
2022-12-30 00:22:32 +0100

    stabalized website, added contact form

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-# from wtforms import BooleanField
+from wtforms import StringField, EmailField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, length, Email
 
 
 class ContactForm(FlaskForm):
-    name = StringField("Name")
-    email = StringField("Email")
-    subject = StringField("Subject")
-    message = TextAreaField("Message")
-    submit = SubmitField("Send")
+    name = StringField("Name", validators=[DataRequired(), length(min=3, max=20)])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    subject = StringField("Subject", validators=[DataRequired(), length(min=3, max=20)])
+    message = TextAreaField("Message", validators=[DataRequired(), length(min=3, max=1000)])
+    submit = SubmitField("Submit")
