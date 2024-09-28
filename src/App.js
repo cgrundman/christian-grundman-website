8f6f301e1eca6b77edd12060c7289165df9ccc56
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-09-28 18:56:49 +0200

    add project management logo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import electricalLogo from './electrical.svg'
 import mechanicalLogo from './mechanical.svg'
 import programmingLogo from './programming.svg'
-// import projectManagementLogo from './programming.svg'
+import projectManagementLogo from './project_management.svg'
 import './App.css';
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
         <img src={electricalLogo} className="App-logo" alt="electricalLogo" />
         <img src={mechanicalLogo} className="App-logo" alt="mechanicalLogo" />
         <img src={programmingLogo} className="App-logo" alt="programmingLogo" />
+        <img src={projectManagementLogo} className="App-logo" alt="projectManagementLogo" />
         <p>
           New site coming soon...
         </p>
