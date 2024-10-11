c62a0abee6a7fd161e79ebf2d56afc75d0a06635
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-11 14:53:05 +0200

    add project page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import ComingSoon from "./pages/ComingSoon.js"
 import HomePage from "./pages/HomePage.js";
 import Projects from "./pages/Projects.js";
+import Project from "./pages/Project.js";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
       { index: true, element: <ComingSoon /> },
       { path: 'homepage', element: <HomePage /> },
       { path: 'projects', element: <Projects /> },
+      { path: 'projects/project', element: <Project /> },
     ],
   }
 ])
---
+++ src/pages/Project.js
@@ -0,0 +1,5 @@
+function Project() {
+    return <h1>Project Page</h1>
+}
+
+export default Project;
