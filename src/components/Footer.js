775bcbebb6c57aafad0f9293c6bae49d0fe804f8
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-11 19:27:22 +0200

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import classes from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={classes.footer}>
+            <p>Footer</p>
+        </footer>
+    )
+}
+
+export default Footer;
