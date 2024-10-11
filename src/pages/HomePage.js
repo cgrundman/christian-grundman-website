f0cc27e22516006b4291778e36e7f542407fc77f
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-11 15:26:14 +0200

    link homepage to projects page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,16 @@
-import PageContent from "../components/PageContent.js"
+import { Link } from "react-router-dom";
+
+import PageContent from "../components/PageContent.js";
 
 function HomePage() {
     return (
         <PageContent title="Home Page">
             <p>Site coming soon!</p>
+            <Link to={`/projects`}>
+                <div>
+                    <p>Projects</p>
+                </div>
+            </Link>
         </PageContent>
     )
 }
