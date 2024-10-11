36c2398a0f88a55748e893891ada3f335ae1582e
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-11 19:27:58 +0200

    add logos to homepage and footer to all pages

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom";
 
+import electricalLogo from '../logos/electrical.svg'
+import mechanicalLogo from '../logos/mechanical.svg'
+import programmingLogo from '../logos/programming.svg'
+import projectManagementLogo from '../logos/project_management.svg'
 import PageContent from "../components/PageContent.js";
 
 function HomePage() {
     return (
         <PageContent title="Home Page">
-            <p>Site coming soon!</p>
+            <div>
+                <img src={electricalLogo} className="Logo" alt="electricalLogo" />
+                <img src={mechanicalLogo} className="Logo" alt="mechanicalLogo" />
+                <img src={programmingLogo} className="Logo underscore" alt="programmingLogo" />
+                <img src={projectManagementLogo} className="Logo" alt="projectManagementLogo" />
+            </div>
+            <h3>Full site coming soon!</h3>
             <Link to={`/projects`}>
                 <div>
                     <p>Projects</p>
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Header from "../components/Header.js";
+import Footer from "../components/Footer.js";
 
 function RootLayout() {
     return (
@@ -10,6 +11,7 @@ function RootLayout() {
                 {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
                 <Outlet />
             </main>
+            <Footer />
         </>
     )
 }
