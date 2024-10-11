ec3a670a36ab64b0813a01962fa609e04d1b8fdf
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-11 15:02:04 +0200

    create Error page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ComingSoon from "./pages/ComingSoon.js"
 import HomePage from "./pages/HomePage.js";
 import Projects from "./pages/Projects.js";
 import Project from "./pages/Project.js";
+import Error from "./pages/Error.js";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <ComingSoon /> },
       { path: 'homepage', element: <HomePage /> },
+      { path: 'error', element: <Error /> },
       { path: 'projects', element: <Projects /> },
       { path: 'projects/project', element: <Project /> },
     ],
---
+++ src/pages/Error.js
@@ -0,0 +1,10 @@
+function Error() {
+    return (
+        <>
+            <h1>Error</h1>
+            <h3>An error has occured.</h3>
+        </>
+    )
+}
+
+export default Error;
