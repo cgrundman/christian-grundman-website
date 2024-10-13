7db538852a543168b2fcf624e99eb2ccb27f46da
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-13 18:56:43 +0200

    create and attach the logo class

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,15 +5,20 @@ import mechanicalLogo from '../logos/mechanical.svg'
 import programmingLogo from '../logos/programming.svg'
 import projectManagementLogo from '../logos/project_management.svg'
 import PageContent from "../components/PageContent.js";
+import classes from "../components/PageContent.module.css"
 
 function HomePage() {
     return (
         <PageContent>
             <div>
-                <img src={electricalLogo} className="Logo" alt="electricalLogo" />
-                <img src={mechanicalLogo} className="Logo" alt="mechanicalLogo" />
-                <img src={programmingLogo} className="Logo" alt="programmingLogo" />
-                <img src={projectManagementLogo} className="Logo" alt="projectManagementLogo" />
+                <div>
+                    <img src={electricalLogo} className={classes.logo} alt="electricalLogo" />
+                    <img src={mechanicalLogo} className={classes.logo} alt="mechanicalLogo" />
+                </div>
+                <div>
+                    <img src={programmingLogo} className={classes.logo} alt="programmingLogo" />
+                    <img src={projectManagementLogo} className={classes.logo} alt="projectManagementLogo" />
+                </div>
             </div>
             <h3>Full site coming soon!</h3>
             <Link to={`/projects`}>
