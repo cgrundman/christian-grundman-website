71e9d752df18567aedea9f68d1d5a7cdb47a1c38
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-13 18:56:56 +0200

    create and attach the avatar class

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,16 +2,17 @@ import casual from '../avatars/casual.svg'
 import professional from '../avatars/professional.svg'
 import relaxed from '../avatars/relaxed.svg'
 import PageContent from '../components/PageContent';
+import classes from '../components/PageContent.module.css'
 
 function AboutMe() {
     return (
         <PageContent title="About Me">
             <div>
-                <img src={casual} alt="casual" width="200" />
-                <img src={professional} alt="professional" width="200" />
-                <img src={relaxed} alt="relaxed" width="200" />
+                <img src={casual} className={classes.avatar} alt="casual" width="200" />
+                <img src={professional} className={classes.avatar} alt="professional" width="200" />
+                <img src={relaxed} className={classes.avatar} alt="relaxed" width="200" />
             </div>
-            Coming Soon...
+            <h3>Coming Soon...</h3>
         </PageContent>
     )
 }
