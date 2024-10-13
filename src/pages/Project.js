0f19f987204251fde956552c82715cdf95129872
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-13 18:09:17 +0200

    add "back" button to project page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 import PageContent from "../components/PageContent";
 
 function Project() {
     return (
         <PageContent title="Project" >
+            <Link to={`/projects`}>
+                <div>
+                    <button>
+                        <p>Back</p>
+                    </button>
+                </div>
+            </Link>
         </PageContent>
     )
 }
