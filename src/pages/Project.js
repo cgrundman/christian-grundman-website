4a93d6fc3064a7da135de97140dd0f284480e154
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-17 18:47:46 +0200

    add backend content to project page

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import PageContent from "../components/PageContent";
 
 function Project() {
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        fetch("/members").then(
+            res => res.json()
+        ).then(
+            data => {
+                setData(data)
+                console.log(data)
+            }
+        )
+    }, [])
+
     return (
         <PageContent title="Project" >
+            <div>
+                {(typeof data.members === 'undefined') ? (
+                    <p>Loading...</p>
+                ) : (
+                    data.members.map((member, i) => (
+                        <p key={i}>{member}</p>
+                    ))
+                )}
+            </div>
             <Link to={`/projects`}>
                 <div>
                     <button>
