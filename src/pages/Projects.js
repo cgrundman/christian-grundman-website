16046746edb10e6195df61a763c8668397b46aef
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-18 17:57:20 +0200

    create appearing content onClick

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,64 +1,51 @@
-// import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import content from '../content.json';
-import computerVision from '../logos/computer_vision.svg'
-import ct from '../logos/ct.svg'
-import ecg from '../logos/ecg.svg'
-import isp from '../logos/isp.svg'
-import leaf_disease from '../logos/leaf_disease.svg'
-import mlStethoscope from '../logos/ml_stethoscope.svg'
-import morse from '../logos/morse.svg'
-import nmr from '../logos/nmr.svg'
+import computerVision from '../logos/computer_vision.svg';
+import ct from '../logos/ct.svg';
+// import ecg from '../logos/ecg.svg';
+// import isp from '../logos/isp.svg';
+// import leaf_disease from '../logos/leaf_disease.svg';
+// import mlStethoscope from '../logos/ml_stethoscope.svg';
+// import morse from '../logos/morse.svg';
+// import nmr from '../logos/nmr.svg';
 import PageContent from "../components/PageContent.js";
-import classes from '../components/PageContent.module.css'
+import classes from '../components/PageContent.module.css';
 
 let LOGOSIZE = "125";
 
 function Projects() {
 
-    // const [data, setData] = useState([])
+    // State to track the visibility of the info
+    const [isInfoVisible, setInfoVisible] = useState(false);
 
-    // useEffect(() => {
-    //     fetch("/projects").then(
-    //         res => res.json()
-    //     ).then(
-    //         data => {
-    //             setData(data)
-    //             console.log(data)
-    //         }
-    //     )
-    // }, [])
+    // Set the info to be displayed
+    const [projectTitle, setProjectTitle] = useState([]);
+    const [projectDesc, setProjectDesc] = useState([])
 
-    // fetch(`${process.env.PUBLIC_URL}/data.json`)
-    // .then((response) => response.json())
-    // .then((data) => {
-    //     console.log(data);
-    // });
-
+    // Function to toggle the visibility
+    const toggleInfo = () => {
+        setInfoVisible(!isInfoVisible); // Toggle the state between true and false
+        setProjectTitle(content.projects[0].title)
+        setProjectDesc(content.projects[0].description)
+    };
 
     return (
         <PageContent title="Projects" >
-            <Link to={`/projects/project`}>
-                <div>
-                    <button>
-                        <p>Project Page</p>
-                    </button>
-                </div>
-            </Link>
             <div>
-                <img src={computerVision} className={classes.projectlogo} alt="computerVision" width={LOGOSIZE} />
+                <img src={computerVision} className={classes.projectlogo} alt="computerVision" onClick={toggleInfo} width={LOGOSIZE} />
                 <img src={ct} className={classes.projectlogo} alt="ct" width={LOGOSIZE} />
-                <img src={ecg} className={classes.projectlogo} alt="ecg" width={LOGOSIZE} />
-                <img src={isp} className={classes.projectlogo} alt="isp" width={LOGOSIZE} />
+                {/* <img src={ecg} className={classes.projectlogo} alt="ecg" width={LOGOSIZE} />
+                <img src={isp} className={classes.projectlogo} alt="isp" width={LOGOSIZE} /> */}
             </div>
-            <div>
+            {/* <div>
                 <img src={leaf_disease} className={classes.projectlogo} alt="leaf_disease" width={LOGOSIZE} />
                 <img src={mlStethoscope} className={classes.projectlogo} alt="mlStethoscope" width={LOGOSIZE} />
                 <img src={morse} className={classes.projectlogo} alt="morse" width={LOGOSIZE} />
                 <img src={nmr} className={classes.projectlogo} alt="nmr" width={LOGOSIZE} />
-            </div>
-            <div>
+            </div> */}
+            {/* <div>
                 {(typeof content.projects === 'undefined') ? (
                     <p>Loading...</p>
                 ) : (
@@ -68,6 +55,22 @@ function Projects() {
                             <p key={i}>{project.description}</p>
                         </div>
                     ))
+                )}
+            </div> */}
+            <div>
+                {/* Conditionally rendering the information */}
+                {isInfoVisible && (
+                    <div>
+                        <h2>{projectTitle}</h2>
+                        <p>{projectDesc}</p>
+                        <Link to={`/projects/project`}>
+                            <div>
+                                <button>
+                                    <p>Project Page</p>
+                                </button>
+                            </div>
+                        </Link>
+                    </div>
                 )}
             </div>
         </PageContent>
