9e94e61abda600101c88dbc598056025b2f46355
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-19 14:49:06 +0200

    add rest of projects to projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,16 +4,15 @@ import { useState } from "react";
 import content from '../content.json';
 import cv from '../logos/computer_vision.svg';
 import ct from '../logos/ct.svg';
-// import ecg from '../logos/ecg.svg';
-// import isp from '../logos/isp.svg';
-// import leaf_disease from '../logos/leaf_disease.svg';
-// import mlStethoscope from '../logos/ml_stethoscope.svg';
-// import morse from '../logos/morse.svg';
-// import nmr from '../logos/nmr.svg';
+import ecg from '../logos/ecg.svg';
+import isp from '../logos/isp.svg';
+import ld from '../logos/leaf_disease.svg';
+import mls from '../logos/ml_stethoscope.svg';
+import mct from '../logos/morse.svg';
+import nmr from '../logos/nmr.svg';
 import PageContent from "../components/PageContent.js";
-// import classes from '../components/PageContent.module.css';
+import classes from '../components/PageContent.module.css';
 
-let LOGOSIZE = "125";
 
 function Projects() {
 
@@ -33,27 +32,24 @@ function Projects() {
 
     return (
         <PageContent title="Projects" >
-            {/* <div>
-                <img src={computerVision} className={classes.projectlogo} alt="computerVision" onClick={toggleInfo} width={LOGOSIZE} />
-                <img src={ct} className={classes.projectlogo} alt="ct" width={LOGOSIZE} />
-                <img src={ecg} className={classes.projectlogo} alt="ecg" width={LOGOSIZE} />
-                <img src={isp} className={classes.projectlogo} alt="isp" width={LOGOSIZE} />
-            </div> */}
-            {/* <div>
-                <img src={leaf_disease} className={classes.projectlogo} alt="leaf_disease" width={LOGOSIZE} />
-                <img src={mlStethoscope} className={classes.projectlogo} alt="mlStethoscope" width={LOGOSIZE} />
-                <img src={morse} className={classes.projectlogo} alt="morse" width={LOGOSIZE} />
-                <img src={nmr} className={classes.projectlogo} alt="nmr" width={LOGOSIZE} />
-            </div> */}
             <div>
-                <img src={cv} alt="Computer Vision" onClick={() => toggleInfo(1)} width={LOGOSIZE} />
-                <img src={ct} alt="Computed Tomography" onClick={() => toggleInfo(2)} width={LOGOSIZE} />
+                <img src={cv} alt="Computer Vision" onClick={() => toggleInfo(1)} className={classes.projectlogo} />
+                <img src={ct} alt="Computed Tomography" onClick={() => toggleInfo(2)} className={classes.projectlogo} />
+                <img src={ecg} alt="Electrocardiogram" onClick={() => toggleInfo(3)} className={classes.projectlogo} />
+                <img src={isp} alt="IMG Signal Processor" onClick={() => toggleInfo(4)} className={classes.projectlogo} />
+            </div>
+            <div>
+                <img src={ld} alt="Leaf Disease" onClick={() => toggleInfo(5)} className={classes.projectlogo} />
+                <img src={mls} alt="ML Stethoscope" onClick={() => toggleInfo(6)} className={classes.projectlogo} />
+                <img src={mct} alt="Morse Code Translator" onClick={() => toggleInfo(7)} className={classes.projectlogo} />
+                <img src={nmr} alt="Nuclear Magnetic Resonance" onClick={() => toggleInfo(8)} className={classes.projectlogo} />
             </div>
             {/* Conditionally rendering information for Selected project */}
             {visibleInfo && (
                 <div>
-                <h2>{content.projects[visibleInfo - 1].title}</h2>
-                <p>{content.projects[visibleInfo -1].description}</p>
+                    <h2>{projects[visibleInfo-1].title}</h2>
+                    <p>{projects[visibleInfo-1].description}</p>
+                    <button>Project Page</button>
                 </div>
             )}
         </PageContent>
