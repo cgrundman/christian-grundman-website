510d404b3d6f1766aa8369307542bcd50690194e
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-19 14:53:47 +0200

    create TLA system for project ids

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 // import { Link } from "react-router-dom";
 
 import content from '../content.json';
-import cv from '../logos/computer_vision.svg';
-import ct from '../logos/ct.svg';
+import cvz from '../logos/computer_vision.svg';
+import ctr from '../logos/ct.svg';
 import ecg from '../logos/ecg.svg';
 import isp from '../logos/isp.svg';
-import ld from '../logos/leaf_disease.svg';
+import ldd from '../logos/leaf_disease.svg';
 import mls from '../logos/ml_stethoscope.svg';
 import mct from '../logos/morse.svg';
 import nmr from '../logos/nmr.svg';
@@ -33,16 +33,16 @@ function Projects() {
     return (
         <PageContent title="Projects" >
             <div>
-                <img src={cv} alt="Computer Vision" onClick={() => toggleInfo(1)} className={classes.projectlogo} />
-                <img src={ct} alt="Computed Tomography" onClick={() => toggleInfo(2)} className={classes.projectlogo} />
-                <img src={ecg} alt="Electrocardiogram" onClick={() => toggleInfo(3)} className={classes.projectlogo} />
-                <img src={isp} alt="IMG Signal Processor" onClick={() => toggleInfo(4)} className={classes.projectlogo} />
+                <img src={cvz} onClick={() => toggleInfo(1)} className={classes.projectlogo} alt="Computer Vision" />
+                <img src={ctr} onClick={() => toggleInfo(2)} className={classes.projectlogo} alt="Computed Tomography Reconstruction" />
+                <img src={ecg} onClick={() => toggleInfo(3)} className={classes.projectlogo} alt="Electrocardiogram" />
+                <img src={isp} onClick={() => toggleInfo(4)} className={classes.projectlogo} alt="IMG Signal Processor" />
             </div>
             <div>
-                <img src={ld} alt="Leaf Disease" onClick={() => toggleInfo(5)} className={classes.projectlogo} />
-                <img src={mls} alt="ML Stethoscope" onClick={() => toggleInfo(6)} className={classes.projectlogo} />
-                <img src={mct} alt="Morse Code Translator" onClick={() => toggleInfo(7)} className={classes.projectlogo} />
-                <img src={nmr} alt="Nuclear Magnetic Resonance" onClick={() => toggleInfo(8)} className={classes.projectlogo} />
+                <img src={ldd} onClick={() => toggleInfo(5)} className={classes.projectlogo} alt="Leaf Disease Detection" />
+                <img src={mls} onClick={() => toggleInfo(6)} className={classes.projectlogo} alt="ML Stethoscope" />
+                <img src={mct} onClick={() => toggleInfo(7)} className={classes.projectlogo} alt="Morse Code Translator" />
+                <img src={nmr} onClick={() => toggleInfo(8)} className={classes.projectlogo} alt="Nuclear Magnetic Resonance" />
             </div>
             {/* Conditionally rendering information for Selected project */}
             {visibleInfo && (
