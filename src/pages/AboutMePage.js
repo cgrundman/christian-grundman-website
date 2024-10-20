061ceb0f5b51f0e9b39fbb13f8abc38466a346cf
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-21 01:12:10 +0200

    add images and content to aboutme page

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -1,6 +1,4 @@
 import casual from '../avatars/casual.svg'
-
-import relaxed from '../avatars/relaxed.svg'
 import PageContent from '../components/PageContent';
 import classes from '../components/PageContent.module.css'
 
@@ -9,9 +7,35 @@ function AboutMePage() {
         <PageContent title="About Me">
             <div>
                 <img src={casual} className={classes.avatar} alt="casual" width="200" />
-                <img src={relaxed} className={classes.avatar} alt="relaxed" width="200" />
             </div>
-            <h3>Coming Soon...</h3>
+            <h3>Motivation</h3>
+            <p>I try to experience life while making cool things.</p>
+            <img 
+                src="https://raw.githubusercontent.com/cgrundman/christian-grundman-website/refs/heads/master/src/images/travel.jpg" 
+                alt="travel" 
+                className={classes.interests} 
+            />
+            <img 
+                src="https://raw.githubusercontent.com/cgrundman/christian-grundman-website/refs/heads/master/src/images/hiking.JPG" 
+                alt="hiking"
+                className={classes.interests} 
+            />
+            <img 
+                src="https://raw.githubusercontent.com/cgrundman/christian-grundman-website/refs/heads/master/src/images/beer.jpg" 
+                alt="beer"
+                className={classes.interests} 
+            />
+            <div className={classes.content}>
+                <h2>Travel Maps</h2>
+                <div>
+                    <h3>US National Parks</h3>
+                    <img src="https://github.com/cgrundman/travel-map-visualization/blob/main/National_Parks/nps.gif?raw=true" alt="NPS Gif" width="75%" />
+                </div>
+                <div> 
+                    <h3>Germany Travels</h3>
+                    <img src="https://github.com/cgrundman/travel-map-visualization/blob/main/Sehenswuerdigkeiten/de.gif?raw=true" alt="DE Gif" width="50%" />
+                </div>
+            </div>
         </PageContent>
     )
 }
