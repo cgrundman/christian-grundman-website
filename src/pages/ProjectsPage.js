803f718dd450b1ba9a1a60ae05cb768d85d094e7
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-23 13:35:49 +0200

    display tags on the projects page

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -99,12 +99,26 @@ function ProjectsPage() {
         <div className={classes.content}>
           <h2>{projects[visibleInfo - 1].title}</h2>
           <p>{projects[visibleInfo - 1].description}</p>
-          <div>
-            {projects[visibleInfo - 1].tags.map((tag, index) => (
-              <button key={index}  className={classes.pTag}>
-                <p>{tag}</p>
-              </button>
-            ))}
+          <div className={classes.tagspace}>
+            {projects[visibleInfo - 1].tags.map((tag, index) =>
+              tag === "Programming" ? (
+                <button key={index} className={classes.pTag}>
+                  <p>{tag}</p>
+                </button>
+              ) : tag === "Project Management" ? (
+                <button key={index} className={classes.pmTag}>
+                  <p>{tag}</p>
+                </button>
+              ) : tag === "Mechanical" ? (
+                <button key={index} className={classes.mTag}>
+                  <p>{tag}</p>
+                </button>
+              ) : (
+                <button key={index} className={classes.eTag}>
+                  <p>{tag}</p>
+                </button>
+              )
+            )}
           </div>
           <Link to={`/projects/${projects[visibleInfo - 1].id}`}>
             <button>
