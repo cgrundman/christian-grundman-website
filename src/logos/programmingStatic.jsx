ae4e7c8fa134fdd6bbe085bcc0b83a38e59576f1
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-23 11:42:12 +0200

    Create programmingStatic.jsx

---
+++ src/logos/programmingStatic.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+const ProgrammingStatic = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 400 400"
+    {...props}
+  >
+    <circle cx="200" cy="200" r="200" fill="#3C4048" stroke="transparent" />
+    <g fill="transparent" stroke-linecap="round" stroke-linejoin="round">
+      <path
+        stroke="#554b57"
+        stroke-width="12"
+        d="M50 270V100h300v170zm55 30h195"
+      />
+      <path
+        stroke="#554b57"
+        stroke-width="12"
+        d="m120 220-50-30 50-30m20 80 45-110m25 90 50-30-50-30"
+      />
+      <path stroke="#a749ba" stroke-width="5" d="M50 270V100h300v170zm55 30h195" />
+      <path
+        stroke="#a749ba"
+        stroke-width="5"
+        d="m120 220-50-30 50-30m20 80 45-110m25 90 50-30-50-30"
+      />
+      <path stroke="#554b57" stroke-width="12" d="M280 240h50" />
+      <path stroke="#a749ba" stroke-width="5" d="M280 240h50" />
+    </g>
+  </svg>
+);
+export default ProgrammingStatic;
