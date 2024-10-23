bb76514ed8e06b961e6e395a8a54b157e62a6116
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-23 12:03:09 +0200

    implement static and animated logos on home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 
 import professional from "../avatars/professional.svg";
 import Electrical from "../logos/electrical.jsx";
+import ElectricalStatic from "../logos/electricalStatic.jsx";
+import MechanicalStatic from "../logos/mechanicalStatic.jsx";
 import Mechanical from "../logos/mechanical.jsx";
+import ProgrammingStatic from "../logos/programmingStatic.jsx";
 import Programming from "../logos/programming.jsx";
-import ProjectManagement from "../logos/project_management.jsx";
+import ProjectManagementStatic from "../logos/projectManagementStatic.jsx";
 import ProjectManagement from "../logos/projectManagement.jsx";
 import PageContent from "../components/PageContent.js";
 import classes from "../components/PageContent.module.css";
@@ -41,26 +44,58 @@ function HomePage() {
           </p>
         </div>
         <div>
-          <Electrical
-            className={visibleInfo === 2 ? classes.logo : classes.logo}
-            onClick={() => toggleInfo(1)}
-            alt="electricalLogo"
-          />
-          <Mechanical
-            className={visibleInfo === 1 ? classes.logo : classes.logo}
-            onClick={() => toggleInfo(2)}
-            alt="mechanicalLogo"
-          />
-          <Programming
-            className={visibleInfo === 3 ? classes.logo : classes.logo}
+          {visibleInfo === 1 ? (
+            <Electrical
+              className={classes.logoAnimated}
+              onClick={() => toggleInfo(1)}
+              alt="electricalLogo"
+            />
+          ) : (
+            <ElectricalStatic
+              className={classes.logo}
+              onClick={() => toggleInfo(1)}
+              alt="electricalStaticLogo"
+            />
+          )}
+          {visibleInfo === 2 ? (
+            <Mechanical
+              className={classes.logoAnimated}
+              onClick={() => toggleInfo(2)}
+              alt="mechanicalLogo"
+            />
+          ) : (
+            <MechanicalStatic
+              className={classes.logo}
+              onClick={() => toggleInfo(2)}
+              alt="mechanicalStaticLogo"
+            />
+          )}
+          {visibleInfo === 3 ? (
+            <Programming
+            className={classes.logoAnimated}
             onClick={() => toggleInfo(3)}
             alt="programmingLogo"
-          />
-          <ProjectManagement
-            className={visibleInfo === 4 ? classes.logo : classes.logo}
+            />
+          ) : (
+            <ProgrammingStatic
+            className={classes.logo}
+            onClick={() => toggleInfo(3)}
+            alt="programmingStaticLogo"
+            />
+          )}
+          {visibleInfo === 4 ? (
+            <ProjectManagement
+            className={classes.logoAnimated}
             onClick={() => toggleInfo(4)}
             alt="projectManagementLogo"
           />
+          ) : (
+            <ProjectManagementStatic
+            className={classes.logo}
+            onClick={() => toggleInfo(4)}
+            alt="projectManagementStaticLogo"
+          />
+          )}
         </div>
       </div>
       {/* Conditionally rendering information for Selected subject */}
