ea3102b37e773576308b88ac24166678011f0033
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-23 12:02:52 +0200

    Create projectManagementStatic.jsx

---
+++ src/logos/projectManagementStatic.jsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+const ProjectManagementStatic = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400" {...props}>
+    <defs>
+      <g id="largeStatic">
+        <circle cx="200" cy="200" r="18" fill="#574c42" stroke="transparent" />
+        <circle cx="200" cy="200" r="14" fill="#ed9e49" stroke="transparent" />
+        <circle cx="200" cy="200" r="10" fill="#574c42" stroke="transparent" />
+        <circle cx="200" cy="200" r="6" fill="#3C4048" stroke="transparent" />
+      </g>
+      <g id="mediumStatic">
+        <circle cx="200" cy="200" r="12" fill="#574c42" stroke="transparent" />
+        <circle cx="200" cy="200" r="8" fill="#ed9e49" stroke="transparent" />
+        <circle cx="200" cy="200" r="4" fill="#574c42" stroke="transparent" />
+      </g>
+      <g id="smallStatic">
+        <circle cx="200" cy="200" r="8" fill="#574c42" stroke="transparent" />
+        <circle cx="200" cy="200" r="4" fill="#ed9e49" stroke="transparent" />
+      </g>
+    </defs>
+    <circle cx="200" cy="200" r="200" fill="#3C4048" stroke="transparent" />
+    <use href="#largeStatic" x="-51.303" y="140.953" />
+    <use href="#largeStatic" x="-96.418" y="114.906" />
+    <use href="#largeStatic" x="-129.903" y="75" />
+    <use href="#largeStatic" x="-147.721" y="26.047" />
+    <use href="#largeStatic" x="-147.721" y="-26.047" />
+    <use href="#largeStatic" x="-129.903" y="-75" />
+    <use href="#largeStatic" x="-96.418" y="-114.906" />
+    <use href="#largeStatic" x="51.303" y="140.953" />
+    <use href="#largeStatic" x="96.418" y="114.906" />
+    <use href="#mediumStatic" y="-100" />
+    <use href="#mediumStatic" x="42.261" y="-90.63" />
+    <use href="#mediumStatic" x="76.604" y="-64.278" />
+    <use href="#mediumStatic" x="96.592" y="-25.881" />
+    <use href="#mediumStatic" x="98.48" y="17.364" />
+    <use href="#mediumStatic" x="81.915" y="57.357" />
+    <use href="#mediumStatic" x="50" y="86.602" />
+    <use href="#mediumStatic" x="8.715" y="99.619" />
+    <use href="#mediumStatic" x="-34.202" y="93.969" />
+    <use href="#mediumStatic" x="-70.71" y="70.71" />
+    <use href="#mediumStatic" x="-93.969" y="34.202" />
+    <use href="#largeStatic" y="150" />
+    <use href="#largeStatic" x="129.903" y="75" />
+    <use href="#largeStatic" x="147.721" y="26.047" />
+    <use href="#smallStatic" y="50" />
+    <use href="#smallStatic" x="-32.139" y="38.302" />
+    <use href="#smallStatic" x="-49.24" y="8.682" />
+    <use href="#smallStatic" x="-43.301" y="-25" />
+    <use href="#smallStatic" x="-17.101" y="-46.984" />
+    <use href="#smallStatic" x="17.101" y="-46.984" />
+    <use href="#smallStatic" x="43.301" y="-25" />
+  </svg>
+);
+export default ProjectManagementStatic;
