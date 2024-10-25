c81f04cc6f654fa9f7b64f7acf24b1b9e9b608b9
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-25 20:14:18 +0200

    Create ProjectLogoMap.js

---
+++ src/components/ProjectLogoMap.js
@@ -0,0 +1,21 @@
+import CVZ from "../logos/CVZ";
+import CTR from "../logos/CTR";
+import ECG from "../logos/ECG";
+import ISP from "../logos/ISP";
+import LDD from "../logos/LDD";
+import MLS from "../logos/MLS";
+import MCT from "../logos/MCT";
+import NMR from "../logos/NMR";
+
+const ProjectLogoMap = {
+    1: CVZ,
+    2: CTR,
+    3: ECG,
+    4: ISP,
+    5: LDD,
+    6: MLS,
+    7: MCT,
+    8: NMR,
+}
+
+export default ProjectLogoMap;
