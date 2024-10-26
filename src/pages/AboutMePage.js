2c567fca8b6ad87e9220f66f2bdb57aa64e3df0b
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-26 23:49:01 +0200

    randomize quote for about me page

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -1,42 +1,57 @@
-import casual from '../avatars/casual.svg'
-import PageContent from '../components/PageContent';
-import classes from '../components/PageContent.module.css'
+import casual from "../avatars/casual.svg";
+import PageContent from "../components/PageContent";
+import content from "../content.json";
+import classes from "../components/PageContent.module.css";
 
 function AboutMePage() {
-    return (
-        <PageContent title="About Me">
-            <div>
-                <img src={casual} className={classes.avatar} alt="casual" width="200" />
-            </div>
-            <p className={classes.quote}>"Don't take life too seriously. You'll never get out alive."</p>
-            <img 
-                src="https://raw.githubusercontent.com/cgrundman/christian-grundman-website/refs/heads/master/src/images/travel.jpg" 
-                alt="travel" 
-                className={classes.interests} 
-            />
-            <img 
-                src="https://raw.githubusercontent.com/cgrundman/christian-grundman-website/refs/heads/master/src/images/hiking.JPG" 
-                alt="hiking"
-                className={classes.interests} 
-            />
-            <img 
-                src="https://raw.githubusercontent.com/cgrundman/christian-grundman-website/refs/heads/master/src/images/beer.jpg" 
-                alt="beer"
-                className={classes.interests} 
-            />
-            <div className={classes.content}>
-                <h2>Travel Maps</h2>
-                <div>
-                    <h3>US National Parks</h3>
-                    <img src="https://github.com/cgrundman/travel-map-visualization/blob/main/National_Parks/nps.gif?raw=true" alt="NPS Gif" width="75%" />
-                </div>
-                <div> 
-                    <h3>Germany Travels</h3>
-                    <img src="https://github.com/cgrundman/travel-map-visualization/blob/main/Sehenswuerdigkeiten/de.gif?raw=true" alt="DE Gif" width="50%" />
-                </div>
-            </div>
-        </PageContent>
-    )
+  // Extract Quote
+  const quotes = content.quotes[0]["life"];
+  var quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  return (
+    <PageContent title="About Me">
+      <div>
+        <img src={casual} className={classes.avatar} alt="casual" width="200" />
+      </div>
+      <p className={classes.quote}>
+        "{quote}"
+      </p>
+      <img
+        src="https://raw.githubusercontent.com/cgrundman/christian-grundman-website/refs/heads/master/src/images/travel.jpg"
+        alt="travel"
+        className={classes.interests}
+      />
+      <img
+        src="https://raw.githubusercontent.com/cgrundman/christian-grundman-website/refs/heads/master/src/images/hiking.JPG"
+        alt="hiking"
+        className={classes.interests}
+      />
+      <img
+        src="https://raw.githubusercontent.com/cgrundman/christian-grundman-website/refs/heads/master/src/images/beer.jpg"
+        alt="beer"
+        className={classes.interests}
+      />
+      <div className={classes.content}>
+        <h2>Travel Maps</h2>
+        <div>
+          <h3>US National Parks</h3>
+          <img
+            src="https://github.com/cgrundman/travel-map-visualization/blob/main/National_Parks/nps.gif?raw=true"
+            alt="NPS Gif"
+            width="75%"
+          />
+        </div>
+        <div>
+          <h3>Germany Travels</h3>
+          <img
+            src="https://github.com/cgrundman/travel-map-visualization/blob/main/Sehenswuerdigkeiten/de.gif?raw=true"
+            alt="DE Gif"
+            width="50%"
+          />
+        </div>
+      </div>
+    </PageContent>
+  );
 }
 
 export default AboutMePage;
