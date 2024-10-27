15b8dd7f3488ddc45cb7e5e630e136238561ea56
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-27 16:21:08 +0100

    format lego logo for dynamic coloring

--- src/logos/LEG.jsx
+++ src/logos/LEG.jsx
@@ -1,11 +1,10 @@
 import * as React from "react";
-const CT = (props) => (
+const LEG = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400" {...props}>
     <defs>
       <g id="Stud">
         <path
           d="M -10 0 A 20 10 0 0 0 10 20 A 20 10 0 0 0 -10 0 M -22 30 A 20 10 0 0 0 22 30 l 0 -5 M -22 30 l 0 -5"
-          stroke="#aaa"
           stroke-width="12"
           fill="transparent"
           stroke-linecap="round"
@@ -29,7 +28,6 @@ const CT = (props) => (
         <use href="#Stud" x="250" y="110" />
         <path
           d="M 60 240 l 0 60 l 90 60 l 190 -120 l 0 -55 M 150 345 l 0 -45 M 70 245 l 69 46 M 160 292 l 170 -107"
-          stroke="#aaa"
           stroke-width="12"
           fill="transparent"
           stroke-linecap="round"
@@ -43,8 +41,8 @@ const CT = (props) => (
         />
       </g>
     </defs>
-    <circle id="background" cx="200" cy="200" r="200" fill="#384247" />
+    <circle id="background" cx="200" cy="200" r="200" stroke="transparent" />
     <use href="#Brick" x="0" y="-50" />
   </svg>
 );
-export default CT;
+export default LEG;
