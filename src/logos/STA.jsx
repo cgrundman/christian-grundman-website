09a96ea7ab04916a2d6dd9b15c6470bb2fb7bbcd
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-10-27 16:33:20 +0100

    update for dynamic coloring

--- src/logos/STA.jsx
+++ src/logos/STA.jsx
@@ -5,7 +5,6 @@ const STA = (props) => (
       <g id="Microphone">
         <path
           d="M -20 0 l 40 0 M 0 0 l 0 -20 M -30 -50 A 20 20 0 0 0 30 -50 M -15 -50 A 15 15 0 0 0 15 -50 l 0 -30 A 15 15 0 0 0 -15 -80 l 0 30"
-          stroke="#aaa"
           stroke-width="12"
           fill="transparent"
           stroke-linecap="round"
@@ -21,7 +20,6 @@ const STA = (props) => (
       <g id="Sound">
         <path
           d="M -80 -20 l 0 40 M -70 -30 l 0 60 M -60 -10 l 0 20 M -50 -5 l 0 10 M -40 -40 l 0 80 M -30 -15 l 0 30 M -20 -20 l 0 40 M -10 -15 l 0 30 M 0 -5 l 0 10 M 10 -35 l 0 70 M 20 -25 l 0 50 M 30 -10 l 0 20"
-          stroke="#aaa"
           stroke-width="12"
           fill="transparent"
           stroke-linecap="round"
@@ -39,14 +37,12 @@ const STA = (props) => (
           x="0"
           y="0"
           r="50"
-          stroke="#aaa"
           stroke-width="12"
           fill="transparent"
           stroke-linecap="round"
         />
         <path
           d="M -20 0 A 20 20 0 0 0 20 0"
-          stroke="#aaa"
           stroke-width="12"
           fill="transparent"
           stroke-linecap="round"
@@ -73,14 +69,12 @@ const STA = (props) => (
           x="0"
           y="0"
           r="50"
-          stroke="#aaa"
           stroke-width="12"
           fill="transparent"
           stroke-linecap="round"
         />
         <path
           d="M 20 20 A 20 20 0 0 0 -20 20"
-          stroke="#aaa"
           stroke-width="12"
           fill="transparent"
           stroke-linecap="round"
@@ -103,7 +97,7 @@ const STA = (props) => (
         />
       </g>
     </defs>
-    <circle id="background" cx="200" cy="200" r="200" fill="#384247" />
+    <circle id="background" cx="200" cy="200" r="200" stroke="transparent" />
     <use href="#Microphone" x="180" y="250" />
     <use href="#Sound" x="105" y="200" />
     <use href="#Smile" x="320" y="125" />
@@ -113,7 +107,6 @@ const STA = (props) => (
       y1="180"
       x2="260"
       y2="150"
-      stroke="#aaa"
       stroke-width="12"
       fill="transparent"
       stroke-linecap="round"
@@ -123,7 +116,6 @@ const STA = (props) => (
       y1="220"
       x2="260"
       y2="250"
-      stroke="#aaa"
       stroke-width="12"
       fill="transparent"
       stroke-linecap="round"
