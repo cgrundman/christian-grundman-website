aaf3ec41764478c3434f6c034d44087c5f2e894e
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-11-07 01:33:05 +0100

    add esp project to website

--- src/components/ProjectLogoMap.js
+++ src/components/ProjectLogoMap.js
@@ -1,9 +1,10 @@
 import CVZ from "../logos/CVZ";
 import CTR from "../logos/CTR";
 import ECG from "../logos/ECG";
+import ESP from "../logos/ESP";
 import ISP from "../logos/ISP";
 import LDD from "../logos/LDD";
-import LEG from "../logos/LEG"
+import LEG from "../logos/LEG";
 import MLS from "../logos/MLS";
 import MCT from "../logos/MCT";
 import NMR from "../logos/NMR";
@@ -11,17 +12,18 @@ import STA from "../logos/STA";
 import TMV from "../logos/TMV";
 
 const ProjectLogoMap = {
-    1: CVZ,
-    2: CTR,
-    3: ECG,
-    4: ISP,
-    5: LDD,
-    6: LEG,
-    7: MLS,
-    8: MCT,
-    9: NMR,
-    10: STA,
-    11: TMV,
-}
+  1: CVZ,
+  2: CTR,
+  3: ECG,
+  4: ESP,
+  5: ISP,
+  6: LDD,
+  7: LEG,
+  8: MLS,
+  9: MCT,
+  10: NMR,
+  11: STA,
+  12: TMV,
+};
 
 export default ProjectLogoMap;
