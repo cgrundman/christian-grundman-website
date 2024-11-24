42d56efd22f565ac6f2cda941617f19c996f0d2b
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-11-24 18:40:37 +0100

    create generic background

---
+++ src/backgrounds/generic.jsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+const Generic = (props) => (
+    <svg
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 210 50"
+    id="svg1"
+    {...props}
+  >
+    <defs>
+      <linearGradient id="linearGradient1">
+        <stop stopColor="#0a052a" stopOpacity="1;" offset={0} id="stop1" />
+        <stop stopColor="#13154f" stopOpacity="1;" offset={0.35} id="stop3" />
+        <stop stopColor="#544535" stopOpacity="1;" offset={1} id="stop2" />
+      </linearGradient>
+      <linearGradient
+        xlinkHref="#linearGradient1"
+        id="linearGradient2"
+        x1={25}
+        y1={-5}
+        x2={25}
+        y2={50}
+        gradientUnits="userSpaceOnUse"
+      />
+    </defs>
+    <g id="layer1">
+      <rect
+        fill="url(#linearGradient2)"
+        strokeWidth={0.264999}
+        id="rect1"
+        width={210}
+        height={60}
+        x={-5}
+        y={-5}
+      />
+    </g>
+  </svg>
+);
+export default Generic;
