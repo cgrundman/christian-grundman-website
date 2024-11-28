2537df012a05d5547189446c8d72cc6ac66e0b4a
cgrundman <92883095+cgrundman@users.noreply.github.com>
2024-11-28 01:00:39 +0100

    add MAC to logo map

--- src/components/ProjectLogoMap.js
+++ src/components/ProjectLogoMap.js
@@ -5,6 +5,7 @@ import ESP from "../logos/ESP";
 import ISP from "../logos/ISP";
 import LDD from "../logos/LDD";
 import LEG from "../logos/LEG";
+import MAC from "../logos/MAC";
 import MLS from "../logos/MLS";
 import MCT from "../logos/MCT";
 import NMR from "../logos/NMR";
@@ -19,11 +20,12 @@ const ProjectLogoMap = {
   5: ISP,
   6: LDD,
   7: LEG,
-  8: MLS,
-  9: MCT,
-  10: NMR,
-  11: STA,
-  12: TMV,
+  8: MAC,
+  9: MLS,
+  10: MCT,
+  11: NMR,
+  12: STA,
+  13: TMV,
 };
 
 export default ProjectLogoMap;
